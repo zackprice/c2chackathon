168c7fa910d181e0c62c5dab93f5fb1e12f366a8
Bruce Szalwinski <szalwinski@csdisco.com>
2021-10-23 11:33:59 -0500

    use types

--- src/endpoints/professionalism.py
+++ src/endpoints/professionalism.py
@@ -14,15 +14,9 @@ def get(text: str):
     """
     client = ComprehendClient()
     response = client.classify_document(text=text, endponint_arn="arn:aws:comprehend:us-east-1:768862600548:document-classifier-endpoint/ProfesionalismChecker")
-    classes = response["Classes"]
+    classes = response.Classes
     for clazz in classes:
-        if clazz["Name"] == "PROFESSIONAL" and clazz["Score"] >= 0.65:
-            return{"result": f"Your Professionalism score is: {clazz['Score']*100} ! This means that your resume is on the right track!"}
+        if clazz.Name == "PROFESSIONAL" and clazz.Score >= 0.65:
+            return{"result": f"Your Professionalism score is: {clazz.Score*100} ! This means that your resume is on the right track!"}
         else:
-            return{"result": f"Your Professionalism score is:  {clazz['Score']*100}! To improve your score check out our resources on our Resources tab!"}
-            
-            
-
-
-
-    return {"response": response}
+            return{"result": f"Your Professionalism score is:  {clazz.Score*100}! To improve your score check out our resources on our Resources tab!"}
