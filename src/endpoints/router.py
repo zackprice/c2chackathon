29c972daa7caa095ab46b3b3881c7cbd2b3f1f66
Ed Mamerto <edmamerto@gmail.com>
2021-10-23 10:58:38 -0400

    wrote the skeleton for the api endpoints (#1)

--- src/endpoints/router.py
+++ src/endpoints/router.py
@@ -1,7 +1,13 @@
 from fastapi import APIRouter
 
-from endpoints import sample, training
-
+from endpoints import sample
+from endpoints import training
+from endpoints import grammar
+from endpoints import spellcheck
+from endpoints import professionalism
+from endpoints import textcount
+from endpoints import resumescore
+from endpoints import personalinfo
 
 def api_router() -> APIRouter:
     router = APIRouter()
@@ -11,6 +17,22 @@ def api_router() -> APIRouter:
     router.include_router(
         training.router, prefix="/training", tags=["training"]
     )
+    router.include_router(
+        grammar.router, prefix="/grammar", tags=["grammar"]
+    )
+    router.include_router(
+        spellcheck.router, prefix="/spellcheck", tags=["spellcheck"]
+    )
+    router.include_router(
+        professionalism.router, prefix="/professionalism", tags=["professionalism"]
+    )
+    router.include_router(
+        textcount.router, prefix="/textcount", tags=["textcount"]
+    )
+    router.include_router(
+        resumescore.router, prefix="/resumescore", tags=["resumescore"]
+    )
+    router.include_router(
+        personalinfo.router, prefix="/personalinfo", tags=["personalinfo"]
+    )
     return router
-
-
---
+++ src/endpoints/spellcheck.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+
+from config import get_settings
+
+router = APIRouter()
+
+
+@router.get("/", summary="validates spellcheck")
+def get():
+    """
+    This is the description
+    """
+    return {"result": "OK", "version": get_settings().VERSION}
